324b55869aa6c4f890a3c6b4034d4e2e025efb71
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-03-24 08:27:59 +0300

    Lessons_1
    Lessons_1

---
+++ Lesson_1.js
@@ -0,0 +1,12 @@
+// Упражнение 1
+var Tc = prompt("Введите градусы по Цельсию: ");
+var Tf
+Tf = (9 / 5) * Tc + 32
+alert(Tf)
+// Упражнение 2
+var admin
+var name = "Василий"
+admin = name
+alert(admin)
+// Упражнение 3
+alert( 1000 + "108")
